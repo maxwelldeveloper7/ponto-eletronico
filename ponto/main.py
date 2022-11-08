80c67bbf12798be72e9f6922570e88263fc5e64d
maxwelldeveloper7 <maxwellchaves1844@gmail.com>
2022-11-08 04:33:35 -0300

    desfazendo codigo de teste

--- ponto/main.py
+++ ponto/main.py
@@ -6,11 +6,5 @@ principal = TelaPrincipal()
 
 
 if (__name__ == "__main__"):
+    principal.exibe_menu()
     principal.clear()
-    colaborador = Colaborador('007702', '04960780622', '1900833194-5',
-                          'maxwell de oliveira chaves',
-                          'instrutor de informática',
-                          '24/07/2013')
-    print(colaborador)
-    # principal.exibe_menu()
-    # principal.clear()
