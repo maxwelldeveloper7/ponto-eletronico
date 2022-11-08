44b713295d8004eea10c5136563b741f485abd21
maxwelldeveloper7 <maxwellchaves1844@gmail.com>
2022-11-08 10:23:00 -0300

    criando classe tela_registros

---
+++ ponto/view/tela_registros.py
@@ -0,0 +1,26 @@
+import datetime
+
+from view.tela import Tela
+
+
+class TelaRegistros(Tela):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def exibir_menu(self):
+        opcao = ""
+        while (True):
+            self.escreve_titulo()
+            self.exibir_data_hora()
+            matricula = int(input("Digite o número de sua matrícula: "))
+            print(matricula)
+            opcao = input(
+                "Deseja realizar outro registro?(S para sim ou enter para sair):"
+                ).lower()
+            if opcao != "s":
+                break
+
+    def exibir_data_hora(self):
+        data = datetime.datetime.now()
+        data_texto = data.strftime('Data e Hora: %d/%m/%Y - %H:%M')
+        print(data_texto)
