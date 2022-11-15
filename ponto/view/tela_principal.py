6f6211f56d5f2983c8dc1d8ff35e54cae8279cac
maxwelldeveloper7 <maxwellchaves1844@gmail.com>
2022-11-15 16:08:49 -0300

    tratamento de entrada de dados com regex

--- ponto/view/tela_principal.py
+++ ponto/view/tela_principal.py
@@ -1,3 +1,5 @@
+import re
+
 from view.tela import Tela
 from view.tela_cadastros import TelaCadastros
 from view.tela_registros import TelaRegistros
@@ -8,19 +10,34 @@ class TelaPrincipal(Tela):
         super().__init__()
 
     def exibe_menu(self):
-        opcao = 0
+        opcao: int = 0
+        mensagem: str = ""
         while (True):
+            # Menu
             self.escreve_titulo()
             print("Menu\n")
             print("1 - Registrar")
             print("2 - Cadastros")
             print("3 - Sair\n")
-            opcao = int(input("Digite uma opção: "))
-            if opcao == 1:
-                tela_registros  = TelaRegistros()
-                tela_registros.exibir_menu()
-            elif opcao == 2:
-                tela_cadastros = TelaCadastros()
-                tela_cadastros.exibir_menu()
-            elif opcao == 3:
-                break
+            # Tratamento do que é informado pelo teclado utilizando regex
+            entrada: str = input(f"{mensagem}Digite uma opção: ")
+            # O valor deve ser entre 1 a 3 inclusive, e deve conter apenas
+            # um caractere
+            padrao: str = "[1-3]"
+            opcao_valida: bool = re.search(padrao, entrada) and len(
+                entrada) == 1
+            # Verifica se a opção é válida
+            if opcao_valida:
+                # reinicia conteúdo da mensagem
+                mensagem: str = ""
+                opcao = int(entrada)
+                if opcao == 1:
+                    tela_registros = TelaRegistros()
+                    tela_registros.exibir_menu()
+                elif opcao == 2:
+                    tela_cadastros = TelaCadastros()
+                    tela_cadastros.exibir_menu()
+                elif opcao == 3:
+                    break
+            else:
+                mensagem = "Opção inválida!!!\n"
