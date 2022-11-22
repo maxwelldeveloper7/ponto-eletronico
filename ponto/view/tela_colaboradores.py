26e1b2079696754820d2895b92a523838263a8b0
maxwelldeveloper7 <maxwellchaves1844@gmail.com>
2022-11-21 21:58:28 -0300

    concluindo recebe_data

--- ponto/view/tela_colaboradores.py
+++ ponto/view/tela_colaboradores.py
@@ -178,7 +178,22 @@ class TelaColaboradores(Tela):
             if mes in range(1,13):
                 if dia in range(1,32):
                     # verifica de a data é válida
-                    ...
+                    # Meses com 31 dias
+                    if (mes == 1 or mes == 3 or mes == 5 or mes == 7 or \
+                        mes == 8 or mes == 10 or mes == 12):
+                        if(dia <= 31):
+                            data_valida = True
+                    # Meses com 30 dias
+                    elif (mes == 4 or mes == 6 or mes == 9 or mes == 11):
+                        if(dia<=30):
+                            data_valida = True
+                    elif mes == 2:
+                        # Testa se é bissexto
+                        if(ano%4 == 0 and ano%100 !=0) or (ano%400 == 0):
+                            if(dia<=29):
+                                data_valida = True
+                        elif(dia<=28):
+                            data_valida = True
                 else:
                     print("dia inválido")
                     return None
