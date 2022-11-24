cddef72879896ea99bd22c5134baa5a914548544
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2022-11-24 10:53:51 -0300

    desenvolvendo teste

--- tests/test_entrada.py
+++ tests/test_entrada.py
@@ -1,4 +1,10 @@
 from ponto.view.entrada import Entrada
 
 class TestClass:
-    ...
+    def test_quando_matricula_for_0_deve_retornar_000000(self):
+        entrada = "0"# Given - contexto
+        esperado = "000000"
+        entrada_teste = Entrada()
+        resultado = entrada_teste.recebe_matricula()# When - ação
+        print(entrada)
+        assert resultado == esperado# Then - desfecho
