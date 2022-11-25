b1ae07fd8f78be0c8f9af10215bcf5ccfcb57658
maxwelldeveloper7 <maxwellchaves1844@gmail.com>
2022-11-24 22:13:32 -0300

    apanhando durante 4 horas para aprender usar input no teste

--- tests/test_entrada.py
+++ tests/test_entrada.py
@@ -1,10 +1,15 @@
 from ponto.view.entrada import Entrada
+from pytest import MonkeyPatch
 
 class TestClass:
     def test_quando_matricula_for_0_deve_retornar_000000(self):
-        entrada = "0"# Given - contexto
+        # Given - contexto
+        monkeypatch = MonkeyPatch()
+        monkeypatch.setattr('builtins.input', lambda _: "0")
         esperado = "000000"
+        # When - ação
         entrada_teste = Entrada()
-        resultado = entrada_teste.recebe_matricula()# When - ação
-        print(entrada)
-        assert resultado == esperado# Then - desfecho
+        resultado = entrada_teste.recebe_matricula()
+        # Then - desfecho
+        assert resultado == esperado
+        
