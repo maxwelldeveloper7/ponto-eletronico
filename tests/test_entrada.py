e2b08862caf03c7ebc87eb85d1a97c3290ef14e8
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2022-11-25 16:42:31 -0300

    mais testes

--- tests/test_entrada.py
+++ tests/test_entrada.py
@@ -1,18 +1,7 @@
 from ponto.view.entrada import Entrada
 from pytest import MonkeyPatch
 
-class TestClass:
-    def test_quando_matricula_for_0_deve_retornar_000000(self):
-        # Given - contexto
-        monkeypatch = MonkeyPatch()
-        monkeypatch.setattr('builtins.input', lambda _: "0")
-        esperado = "000000"
-        # When - ação
-        entrada_teste = Entrada()
-        resultado = entrada_teste.recebe_matricula()
-        # Then - desfecho
-        assert resultado == esperado
-
+class TestRecebeMatricula:
     def test_quando_matricula_for_vazia_deve_retornar_None(self):
         # Given - contexto
         monkeypatch = MonkeyPatch()
@@ -23,4 +12,80 @@ class TestClass:
         resultado = entrada_teste.recebe_matricula()
         # Then - desfecho
         assert resultado == esperado
-        
+
+    def test_quando_matricula_for_0_deve_retornar_None(self):
+        # Given - contexto
+        monkeypatch = MonkeyPatch()
+        monkeypatch.setattr('builtins.input', lambda _: "0")
+        esperado = None
+        # When - ação
+        entrada_teste = Entrada()
+        resultado = entrada_teste.recebe_matricula()
+        # Then - desfecho
+        assert resultado == esperado
+
+    def test_quando_matricula_for_9999999_deve_retornar_None(self):
+        # Given - contexto
+        monkeypatch = MonkeyPatch()
+        monkeypatch.setattr('builtins.input', lambda _: "9999999")
+        esperado = "None"
+        # When - ação
+        entrada_teste = Entrada()
+        resultado = entrada_teste.recebe_matricula()
+        # Then - desfecho
+        assert resultado == esperado
+
+    def test_quando_matricula_for_1_deve_retornar_000001(self):
+        # Given - contexto
+        monkeypatch = MonkeyPatch()
+        monkeypatch.setattr('builtins.input', lambda _: "1")
+        esperado = "000001"
+        # When - ação
+        entrada_teste = Entrada()
+        resultado = entrada_teste.recebe_matricula()
+        # Then - desfecho
+        assert resultado == esperado
+
+    def test_quando_matricula_for_0000001_deve_retornar_None(self):
+        # Given - contexto
+        monkeypatch = MonkeyPatch()
+        monkeypatch.setattr('builtins.input', lambda _: "0000001")
+        esperado = None
+        # When - ação
+        entrada_teste = Entrada()
+        resultado = entrada_teste.recebe_matricula()
+        # Then - desfecho
+        assert resultado == esperado
+
+    def test_quando_matricula_for_a000001_deve_retornar_000001(self):
+        # Given - contexto
+        monkeypatch = MonkeyPatch()
+        monkeypatch.setattr('builtins.input', lambda _: "a000001")
+        esperado = "000001"
+        # When - ação
+        entrada_teste = Entrada()
+        resultado = entrada_teste.recebe_matricula()
+        # Then - desfecho
+        assert resultado == esperado
+
+    def test_quando_matricula_for_a1_deve_retornar_000001(self):
+        # Given - contexto
+        monkeypatch = MonkeyPatch()
+        monkeypatch.setattr('builtins.input', lambda _: "a1")
+        esperado = "000001"
+        # When - ação
+        entrada_teste = Entrada()
+        resultado = entrada_teste.recebe_matricula()
+        # Then - desfecho
+        assert resultado == esperado
+
+    def test_quando_matricula_for_a999999_deve_retornar_999999(self):
+        # Given - contexto
+        monkeypatch = MonkeyPatch()
+        monkeypatch.setattr('builtins.input', lambda _: "a999999")
+        esperado = "999999"
+        # When - ação
+        entrada_teste = Entrada()
+        resultado = entrada_teste.recebe_matricula()
+        # Then - desfecho
+        assert resultado == esperado
