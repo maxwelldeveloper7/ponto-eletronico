488ea14c6d369e560a5355c56fd4597e5326f2ae
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2023-05-02 09:44:12 -0300

    metodo formata_nome

--- ponto/utilitarios.py
+++ ponto/utilitarios.py
@@ -160,6 +160,22 @@ class Entrada:
         print('Data inválida ->', data)
         return None
 
+    @staticmethod
+    def formatar_nome(nome_completo: str) -> str:
+        # Converte o nome completo para minúsculo e depois aplica o método title() para colocar a primeira letra de cada palavra em maiúscula
+        nome_formatado = nome_completo.lower().title()
+
+        # Substitui as ocorrências das preposições pela mesma palavra em minúsculo
+        nome_formatado = (
+            nome_formatado.replace(" De ", " de ")
+            .replace(" Da ", " da ")
+            .replace(" Das ", " das ")
+            .replace(" Do ", " do ")
+            .replace(" Dos ", " dos ")
+        )
+
+        return nome_formatado   
+
 
 class Validar:
     """Realiza validações de datas"""
